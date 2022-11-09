6d46f104b866d84b80aa7ab8c9e919e9b3269066
TenKaiMass <mass.merouane@gamil.com>
2022-11-09 11:40:20 +0100

    recuperation de la note + anti detection niv1 + recuperation des produits sur les 5 premières pages

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Scrap_amazon
-Recupere la liste des produits classé soit par le prix soit la note attribuer
+Le but est de Recupèrer la liste des produits suite une recherche  
 Nous allons alors scraper amazon el leader du e-commerce
+et faire un graphe facilitant le choix du produit à acheter
 
 # Le dataset
-Il sera generer au fil du projet
+Il sera generer au fil du projet, il reprendra les produits d'une categorie en passant dans les 5 première page
+
+
+
+
+
 
--- app.py
+++ app.py
@@ -1,44 +1,46 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
-from urllib import request
+
 
 session = HTMLSession()
 produit="camera+vlog"
-url = f"https://www.amazon.fr/s?k={produit}&i=electronics&page=1"
+headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/71.0.3542.0 Safari/537.36"}
+#url =  "https://httpbin.org/user-agent"
+proxies = {
+"http": "http://49.0.2.242:8090",
+"https": "https://49.0.2.242:8090",
+ }
 
-
-def get_date(url):
-    r = session.get(url)
+def getPageRH(url,proxies):
+    r = session.get(url,timeout=2)
     r.html.render(sleep=1)
     soup = BeautifulSoup(r.html. html, 'html.parser')
     return soup
 
-
-
-def getdeals (soup) :
-    products = soup.find_all('div', {'data-component-type': 's-search-result'})
-    for item in products:
-        #title = item.find('a',{'class': 'a-link-normal a-text-normal'}).text.strip()
-        short_title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()[:25]
-        #link = item.find('a', {'class':'a-link-normal a-text-normal'})['href']
-        print(short_title)
-
-def getPage(url):
-    request_text = request.urlopen(url).read()
-    return BeautifulSoup(request_text, 'lxml')
-
-def getMytitleProduct(url):
-    page = getPage(url)
+def getMytitleProduct(url,proxies):
+    page = getPageRH(url,proxies)
     for item in page.find('div', {'class' : 's-main-slot s-result-list s-search-results sg-row'}).findAll({'div'}) : 
         if item.get("data-component-type") :
             title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()[:25]
-            price = item.find('span',{'class': 'a-price-whole'}).text.strip()
+            try:
+                price = item.find('span',{'class': 'a-price-whole'}).text.strip().replace(',','.')
+            except AttributeError:
+                price = 0
             try:
                 avis = item.find('span',{'class': 'a-size-base puis-light-weight-text s-link-centralized-style'}).text.strip()
             except AttributeError:
                 avis = 0
-            print(title, f"Price : {price}; Avis : {avis}\n-------")
+            try:
+                stars = float(item.find('span',{'class':'a-icon-alt'}).text.strip()[:3].replace(',','.'))
+            except AttributeError:
+                stars = 0
+            print(title, f"Price : {price}; Avis : {avis}; stars : {stars}\n-------")
 #res = get_date(url)
 #getdeals(res)
 
-getMytitleProduct(url)
+for num in range(5):
+    url = f"https://www.amazon.fr/s?k={produit}&i=electronics&page={str(num+1)}"
+    print(f"page {num+1}:\n")
+    getMytitleProduct(url,proxies)
+# url = f"https://www.amazon.fr/s?k={produit}&i=electronics&page=1"
+# getMytitleProduct(url,proxies)
